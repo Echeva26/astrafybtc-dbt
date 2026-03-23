be6cb32d5543e8211ca8e071e00c294f1d74fa45
Echeva26 <jeu2604@gmail.com>
2026-03-23 20:20:31 +0100

    Refactor SQL query in stg_bch_transactions to include a CTE for maximum block_timestamp, enhancing filtering logic to retrieve transactions from the last three months based on the latest timestamp.

--- models/staging/stg_bch_transactions.sql
+++ models/staging/stg_bch_transactions.sql
@@ -1,5 +1,11 @@
 {{ config(materialized="table") }}
 
-select *
-from {{ source("crypto_bitcoin_cash", "transactions") }}
-where block_timestamp >= timestamp(date_sub(current_date(), interval 3 month))
+with latest as (
+    select max(block_timestamp) as max_ts
+    from {{ source("crypto_bitcoin_cash", "transactions") }}
+)
+
+select t.*
+from {{ source("crypto_bitcoin_cash", "transactions") }} t
+cross join latest
+where t.block_timestamp >= timestamp(date_sub(date(latest.max_ts), interval 3 month))
